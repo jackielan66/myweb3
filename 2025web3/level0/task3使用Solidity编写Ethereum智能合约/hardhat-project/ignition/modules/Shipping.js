ee599ed493ea92477afc455a5c0c5cb184816ebd
jackie <1370672560@qq.com>
2025-03-19 20:53:00 +0800

    smoe

--- 2025web3/level0/task3使用Solidity编写Ethereum智能合约/hardhat-project/ignition/modules/Shipping.js
+++
@@ -1,6 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-module.exports = buildModule("ShippingModule", (m) => {
-    const shipping = m.contract("Shipping", []);
-    m.call(shipping, "Status", []);
-    return { shipping };
-});
