c89e38b00e80281202201be4f6242ce22091ad59
jackie <1370672560@qq.com>
2025-03-28 12:17:16 +0800

    first

--- 2025web3/level3/task-stake/stake-front-end/src/utils/env.ts
+++
@@ -1,4 +0,0 @@
-import { Address, zeroAddress } from "viem";
-
-let defaultAddress ='0x01A01E8B862F10a3907D0fC7f47eBF5d34190341'
-export const StakeContractAddress = process.env.NEXT_PUBLIC_STAKE_ADDRESS as Address || defaultAddress || zeroAddress 
