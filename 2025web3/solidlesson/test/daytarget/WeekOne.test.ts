4ad2738a3f2ada2455772405bcbee63b24c0a09d
jackie <1370672560@qq.com>
2025-11-30 22:26:35 +0800

    问题总结

--- 2025web3/solidlesson/test/daytarget/WeekOne.test.ts
+++ 2025web3/solidlesson/test/daytarget/WeekOne.test.ts
@@ -4,13 +4,17 @@ import hre from "hardhat";
 const { ethers } = require("hardhat");
 
 const deploy = async () => {
+    const [owner, user1, user2] = await hre.viem.getWalletClients();
+
     const userRegistry = await hre.viem.deployContract("UserRegistry");
 
     const UserProfile = await hre.viem.deployContract("UserProfile", ["jackie", 30n]);
 
     const VisibilityDemo = await hre.viem.deployContract("VisibilityDemo");
 
-    return { userRegistry, UserProfile, VisibilityDemo };
+    const ErrorHandlingDemo = await hre.viem.deployContract("ErrorHandlingDemo");
+
+    return { userRegistry, UserProfile, VisibilityDemo, ErrorHandlingDemo, user1 };
 };
 
 
@@ -42,22 +46,39 @@ describe("Week 1", function () {
     // });
 
     // 测试函数可见性
-    it("should test visibility 测试函数可见性 day5", async () => {
-        const { VisibilityDemo } = await loadFixture(deploy);
-        // 获取测试账户
-        const [owner] = await hre.viem.getWalletClients();
+    // it("should test visibility 测试函数可见性 day5", async () => {
+    //     const { VisibilityDemo } = await loadFixture(deploy);
+    //     // 获取测试账户
+    //     const [owner] = await hre.viem.getWalletClients();
 
-        expect(await VisibilityDemo.read.getBase()).to.equal(100n);
+    //     expect(await VisibilityDemo.read.getBase()).to.equal(100n);
 
-        expect(await VisibilityDemo.read.getSecretExternal()).to.equal(42n);
+    //     expect(await VisibilityDemo.read.getSecretExternal()).to.equal(42n);
 
-        const r = await VisibilityDemo.read.compute();
-        // a = baseValue + 10 = 110
-        // b = secretNumber * 2 = 84
-        expect(r[0]).to.equal(110n);
-        expect(r[1]).to.equal(84);
+    //     const r = await VisibilityDemo.read.compute();
+    //     // a = baseValue + 10 = 110
+    //     // b = secretNumber * 2 = 84
+    //     expect(r[0]).to.equal(110n);
+    //     expect(r[1]).to.equal(84);
 
-    })
+    // })
+
+    // ErrorHandlingDemo 错误边界处理
+    it("should test error handling 错误边界处理 day5", async () => {
+        const { ErrorHandlingDemo, user1 } = await loadFixture(deploy);
+        // 错误边界处理    
+        // 使用 Hardhat 的异常断言 expect(.to.be.rejectedWith('Value must be 1 or 2');
+        await expect(ErrorHandlingDemo.write.setValue([0n], {
+            account: user1.account.address,
+        })).to.be.rejectedWith('金额必须大于- Value must be > 0"')
+        // evertedWith("Value must be > 0");
+
+
+
+
+    });
+
+
 });
 
 
